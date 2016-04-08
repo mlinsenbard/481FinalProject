782a29317bf37a4a80060f5b4d986a66143ab2db
mlinsenbard <m.linsenbard@gmail.com>
2016-04-08 10:05:16 -0700

    added consumer keys to python file

--- discog_query_client.py
+++ discog_query_client.py
@@ -3,5 +3,8 @@ import discogs_client
 
 user_agent = "481FinalProject/1.0"
 
+consumer_key = "qBUAEwezrIOsHKBuZXJR"
+consumer_secret = "ZEsJqtIuyoweiVbEZwOPOveIwiNtZQNA"
+
 discogclient = discogs_client.Client(user_agent)
 
